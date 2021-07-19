37d3e7ec3099ab85d074a37caf499a56fd6edccc
Markus <mysecondworld@firesurf.de>
2021-07-19 14:41:07 +0200

    Add CCTX huobi global.

--- jesse/modes/import_candles_mode/drivers/huobi_global.py
+++ jesse/modes/import_candles_mode/drivers/huobi_global.py
@@ -5,8 +5,6 @@ from jesse.modes.import_candles_mode.drivers.interface import CandleExchange
 
 class HuobiGlobal(CandleExchange):
     def __init__(self) -> None:
-        # import here instead of the top of the file to prevent possible the circular imports issue
-        from jesse.modes.import_candles_mode.drivers.binance import Binance
 
         super().__init__(
             name='Huobi Global',
@@ -14,10 +12,10 @@ class HuobiGlobal(CandleExchange):
             # See here https://ccxt.readthedocs.io/en/latest/manual.html#ohlcv-candlestick-charts
             count=1000,
             rate_limit_per_second=2,
-            backup_exchange_class=Binance
+            backup_exchange_class=None
         )
 
-        exchange_id = 'huobipro'
+        exchange_id = 'binance'
         # cctx has a built-in rate limiter as alternative self.exchange_class.rateLimit returns the exchanges limit.
         # all properties of the exchange class: https://ccxt.readthedocs.io/en/latest/manual.html#exchange-structure
         self.exchange_class = getattr(ccxt, exchange_id)({'enableRateLimit': True})
@@ -35,9 +33,15 @@ class HuobiGlobal(CandleExchange):
         symbol = symbol.replace("-", "/")
 
         try:
-            w_first_timestamp = self.exchange_class.fetch_ohlcv(symbol, '1week', 1230768000000)[0][0]
-            d_first_timestamp = self.exchange_class.fetch_ohlcv(symbol, '1day', w_first_timestamp, limit=1)[0][0]
-            m_first_timestamp = self.exchange_class.fetch_ohlcv(symbol, '1m', d_first_timestamp, limit=1)[0][0]
+            first_timestamp = None
+            # here we loop through all timeframes starting with the biggest to find the first timestamp in a fast dynamic way.
+            for timeframe in reversed(list(self.exchange_class.timeframes.keys())):
+                if not first_timestamp:
+                    first_timestamp = self.exchange_class.fetch_ohlcv(symbol, timeframe, since=1230768000000)[0][0]
+                else:
+                    first_timestamp = self.exchange_class.fetch_ohlcv(symbol, timeframe, since=first_timestamp, limit=2)[0][0]
+                if timeframe == "1m":
+                    break
         except ccxt.NetworkError as e:
             raise ValueError(self.exchange_class.id, 'get_starting_time failed due to a network error:', str(e))
         except ccxt.ExchangeError as e:
@@ -47,8 +51,7 @@ class HuobiGlobal(CandleExchange):
 
         # since the first timestamp doesn't include all the 1m
         # candles, let's start since the second day then
-        first_timestamp = int(m_first_timestamp)
-        second_timestamp = first_timestamp + 60_000 * 1440
+        second_timestamp = int(first_timestamp) + 60_000 * 1440
 
         return second_timestamp
 
@@ -56,7 +59,7 @@ class HuobiGlobal(CandleExchange):
 
         # cctx doesn't accept an end timestamp but only a count / limit of candles
         try:
-            data = self.exchange_class.fetch_ohlcv(symbol.replace("-", "/"), '1m', start_timestamp, self.count - 1)
+            data = self.exchange_class.fetch_ohlcv(symbol.replace("-", "/"), '1m', since=start_timestamp, limit=self.count - 1)
         except ccxt.NetworkError as e:
             raise ValueError(self.exchange_class.id, 'fetch failed due to a network error:', str(e))
         except ccxt.ExchangeError as e:
@@ -65,7 +68,6 @@ class HuobiGlobal(CandleExchange):
             raise ValueError(self.exchange_class.id, 'fetch failed with:', str(e))
 
         candles = []
-
         for d in data:
             candles.append({
                 'id': jh.generate_unique_id(),

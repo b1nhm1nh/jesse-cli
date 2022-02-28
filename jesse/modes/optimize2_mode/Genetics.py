1841005d1ede6aa691105525cfd23201374c9ce0
B1nh M1nh <binhminhbeu@gmail.com>
2022-02-28 18:24:28 +0700

    Update Genetics.py

--- jesse/modes/optimize2_mode/Genetics.py
+++ jesse/modes/optimize2_mode/Genetics.py
@@ -177,10 +177,11 @@ class Genetics(ABC):
             replace_at = randint(0, self.solution_len - 1)
             baby['dna'] = _baby_dna
             if self.charsets is not None:
-                replace_with = choice(self.charsets[replace_at] - baby['dna'][replace_at])
+                # make sure the new char is not the same as the old one
+                replace_with = choice(self.charsets[replace_at].replace(baby['dna'][replace_at], ''))
             else:
                 # make sure the new char is not the same as the old one
-                replace_with = choice(self.charset - baby['dna'][replace_at])
+                replace_with = choice(self.charset.replace(baby['dna'][replace_at], ''))
             dna = f"{baby['dna'][:replace_at]}{replace_with}{baby['dna'][replace_at + 1:]}"
 
             try:
